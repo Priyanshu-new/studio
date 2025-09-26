4db2cffdeb7ffae321443514ce64879c651587ee
Anushka Mewada <anushkamewada2@gmail.com>
2025-09-26 21:29:04 +0000

    fix the latency for the fix emotion section to detect and react as music

--- src/app/dashboard/fix-emotions/page.tsx
+++ src/app/dashboard/fix-emotions/page.tsx
@@ -101,7 +101,7 @@ export default function GestureControlPage() {
       setIsDetecting(false);
     } else {
       setIsDetecting(true);
-      intervalRef.current = setInterval(detectGesture, 5000); // Check every 5 seconds
+      intervalRef.current = setInterval(detectGesture, 2000); // Check every 2 seconds
     }
   };
 
