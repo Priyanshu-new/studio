6ae907704d56f05a52ee0cc277c5c00554a4b88b
Priyanshu-new <samadhan22222222@gmail.com>
2025-09-28 02:00:17 +0530

    adding the api key

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({AIzaSyAT7VF7OJi_UZElbLmrxz7aYlluBPbMBEA})],
+  plugins: [googleAI({apiKey:AIzaSyAT7VF7OJi_UZElbLmrxz7aYlluBPbMBEA})],
   model: 'googleai/gemini-2.5-flash',
 });
